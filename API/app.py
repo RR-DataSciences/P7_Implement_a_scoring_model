6b6834396b028a7734a0681fab4583af8ebf3561
EC2 Default User <ec2-user@ip-172-31-20-191.eu-west-1.compute.internal>
2024-06-28 09:50:21 +0000

    debug no predict result

--- API/app.py
+++ API/app.py
@@ -26,9 +26,11 @@ def predict():
     # Charger votre modèle ici
     model_path = "/home/ec2-user/P7_Implement_a_scoring_model/models/307511_rawdata_LGBM-[24-06-07 at 18_28].pkl"
     model = joblib.load(model_path)
-    
+    app.logger.debug(f"Model loaded from {model_path}")
+
     scaler_path = '/home/ec2-user/P7_Implement_a_scoring_model/models/scaler_rawdata.pkl'
     scaler = joblib.load(scaler_path)
+    app.logger.debug(f"Scaler loaded from {scaler_path}")
 
     try:
         # Convertir les données JSON en DataFrame
@@ -38,16 +40,18 @@ def predict():
         # Assurer que les colonnes du DataFrame sont dans le même ordre que celles attendues par le modèle
         estimator = model.estimator_
         expected_features = estimator.booster_.feature_name()
-        app.logger.debug(f"columns modèle: {expected_features}")
+        app.logger.debug(f"Expected features: {expected_features}")
+        app.logger.debug(f"DataFrame columns: {df.columns.tolist()}")
         # df = df[expected_features]
         # app.logger.debug(f"Reordered DataFrame: {df}")
+        app.logger.debug(f"Reordered DataFrame: {df}")
 
         # Prétraitement des données si nécessaire
         data_scaled = scaler.transform(df)
 
         # Convertir les données transformées en DataFrame pour faciliter le traitement ultérieur
         df_transformed = pd.DataFrame(data_scaled, columns=df.columns, index=df.index)
-        app.logger.debug(f"df_transformed: {df_transformed}")
+        app.logger.debug(f"Transformed DataFrame: {df_transformed}")
         
         df_transformed = np.array(df_transformed)
 
@@ -63,7 +67,8 @@ def predict():
             'score': score.tolist()
         })
     except Exception as e:
+        app.logger.error(f"Error: {str(e)}")
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=22, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
