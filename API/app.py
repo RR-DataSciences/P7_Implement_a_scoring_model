d9aaacebae0194d86111ba0aa1355a4e5a393106
Rémi <remi.rogulski@gmail.com>
2024-07-19 17:57:43 +0200

    test 6 - MAJ .yml conforme à la doc github + MAJ droit AWS

--- API/app.py
+++ API/app.py
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 @app.route('/')
 def welcome():
     logger.info("Bienvenue sur votre API !")
-    return "V4 - Bienvenue sur votre API !"
+    return "V5 - Bienvenue sur votre API !"
 
 @app.route('/predict', methods=['POST'])
 def predict():
