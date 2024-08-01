b2873491cc4211898691003e26d7b74b28188570
Rémi <remi.rogulski@gmail.com>
2024-08-01 12:12:44 +0200

    test3 maj SSH_PRIVATE_KEY

--- API/app.py
+++ API/app.py
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 @app.route('/')
 def welcome():
     logger.info("Bienvenue sur votre API !")
-    return "V9 - Bienvenue sur votre API !"
+    return "V10 - Bienvenue sur votre API !"
 
 @app.route('/predict', methods=['POST'])
 def predict():
