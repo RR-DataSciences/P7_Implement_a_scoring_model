698360773bc1f33e074b2b689618741c55899a39
Rémi <remi.rogulski@gmail.com>
2024-08-02 10:52:39 +0200

    test - git pull my repo on ec2

--- API/app.py
+++ API/app.py
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 @app.route('/')
 def welcome():
     logger.info("Bienvenue sur votre API !")
-    return "V18 - Bienvenue sur votre API !"
+    return "V19 - Bienvenue sur votre API !"
 
 @app.route('/predict', methods=['POST'])
 def predict():
