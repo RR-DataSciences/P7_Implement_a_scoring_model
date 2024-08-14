d083a9f305034744163168715bb65071ec586287
Rémi <remi.rogulski@gmail.com>
2024-08-14 17:13:12 +0200

    test 16 - deploy API on ec2 with Gunicorn and gnix

--- API/app.py
+++ API/app.py
@@ -35,7 +35,7 @@ with open(model_path, 'rb') as file:
 @app.route('/')
 def welcome():
     logger.info("Bienvenue sur votre API !")
-    return "V47 - Bienvenue sur votre API !"
+    return "V48 - Bienvenue sur votre API !"
 
 @app.route('/predict', methods=['POST'])
 def predict():
