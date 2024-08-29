1d6de2f6d28d73f33bde7e175bf834f1feef09b3
Rémi <remi.rogulski@gmail.com>
2024-08-29 18:41:53 +0200

    MAJ Dashboard - MAJ shap_values, add df_train and select the 3 most important features

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,22 +5,27 @@ import streamlit as st
 import plotly.graph_objs as go
 import plotly.express as px
 import shap
+import matplotlib.pyplot as plt
 
-# Lire le fichier CSV
-path = "C:/Users/remid/Documents/_OC_ParcoursDataScientist/P7_Implémentez_un_modèle_de_scoring/data/API_test"
-data = pd.read_csv(f'{path}/test_imputed_df_api_20.csv', sep=';', index_col='SK_ID_CURR')
+# Lire le fichier CSV - train
+path_train = "C:/Users/remid/Documents/_OC_ParcoursDataScientist/P7_Implémentez_un_modèle_de_scoring/data/API_test"
+df_train = pd.read_csv(f'{path_train}/train_imputed_df_api.csv', sep=';', index_col='SK_ID_CURR')
+
+# Lire le fichier CSV - test
+path_test = "C:/Users/remid/Documents/_OC_ParcoursDataScientist/P7_Implémentez_un_modèle_de_scoring/data/API_test"
+df_test = pd.read_csv(f'{path_test}/test_imputed_df_api_20.csv', sep=';', index_col='SK_ID_CURR')
 
 # Sélecteur pour choisir un individu
-selected_id = st.selectbox("Sélectionnez un identifiant client", data.index)
+selected_id = st.selectbox("Sélectionnez un identifiant client", df_test.index)
 
 # Extraire les données pour l'individu sélectionné
-selected_data = data.loc[[selected_id]]  # On conserve le format DataFrame
+selected_data = df_test.loc[[selected_id]]  # On conserve le format DataFrame
 
 # Convertir en JSON
 data_json = selected_data.to_dict(orient='records')
 
 # URL de l'API
-url = "http://52.214.54.157:5000/predict"
+url = "http://3.254.3.136:5000/predict"
 
 st.write(f"**Version 1**")
 
@@ -56,9 +61,9 @@ if st.button("Faire une prédiction"):
             st.plotly_chart(gauge)
 
             
-            st.write(f"Description du dataset: {data.shape}")
-            st.write(f"Description du dataset: {prediction['rfe_columns']}")
-            st.write(f"Description du dataset: {prediction['shap_details']}")
+            # st.write(f"Description du dataset: {data.shape}")
+            # st.write(f"Description du dataset: {prediction['rfe_columns']}")
+            # st.write(f"Description du dataset: {prediction['shap_details']}")
 
             # Since shap_details is a list, you need to access its first element (or the one you need)
             shap_detail = shap_details[0]  # Assuming you want the first dictionary in the list
@@ -68,7 +73,34 @@ if st.button("Faire une prédiction"):
             base_value = shap_detail['base_value']
             features = np.array(shap_detail['features'])
             feature_names = prediction['rfe_columns']
+            
+            # Vérification initiale des données
+            st.write("Données SHAP disponibles :", len(shap_details))
+            st.write("Nombre de colonnes dans shap_values :", shap_values.shape if hasattr(shap_values, 'shape') else None)
+            st.write("Nombre de colonnes dans feature_names :", len(feature_names))
 
+            # Calcul des scores absolus pour chaque valeur
+            scores_abs = np.abs(shap_values)
+            # st.write("Scores absolus obtenus :", scores_abs)
+
+            # Création d'un DataFrame avec les scores absolus et les noms des caractéristiques
+            df_scores = pd.DataFrame({
+                'feature': feature_names,
+                'score': scores_abs.flatten()  # Flatten l'array 2D en 1D
+            })
+            # Tri des scores par ordre décroissant
+            df_scores_sorted = df_scores.sort_values('score', ascending=False)
+            # st.write("DataFrame des scores absolus (ordre décroissant) :", df_scores_sorted)
+            
+            # Sélection des trois variables les plus influentes
+            top_features = df_scores_sorted.head(3)
+            top_features_list = top_features['feature'].tolist()
+            
+            # Affichage des variables les plus influentes
+            st.write("### Variables les plus influentes pour la prédiction")
+            for index, row in top_features.iterrows():
+                st.write(f"{index+1}. {row['feature']}: SHAP score = {row['score']:.2f}")
+            
             # Creating a SHAP Explanation object
             shap_exp = shap.Explanation(values=shap_values, 
                                         base_values=base_value, 
@@ -77,19 +109,12 @@ if st.button("Faire une prédiction"):
 
             # Displaying the SHAP Force Plot
             st.write(f"**SHAP Force Plot for the selected client ({selected_id}):**")
-            shap.force_plot(shap_exp.base_values, shap_exp.values, shap_exp.data, feature_names=shap_exp.feature_names, matplotlib=True)
+            shap.force_plot(shap_exp.base_values, shap_exp.values, shap_exp.data, feature_names=shap_exp.feature_names, text_rotation=25, matplotlib=True)
+            st.set_option('deprecation.showPyplotGlobalUse', False)
             st.pyplot()
 
-            st.write("Valeur Shape", prediction['shap_values'])
+            st.write(f"Affiche les données d'entrainements: {df_train}")
 
-            # Afficher les valeurs SHAP
-            # shap_values = prediction['shap_values']
-            # shap_df = pd.DataFrame(shap_values, columns=data.columns)
-            
-            # st.write(f"**Valeurs SHAP pour le client sélectionné ({selected_id}):**")
-            # for column in shap_df.columns:
-            #     fig = px.bar(shap_df, x=shap_df.index, y=column, title=f"Impact des caractéristiques sur {column}")
-            #     st.plotly_chart(fig)
         else:
             st.error("Les valeurs SHAP ne sont pas présentes dans la réponse.")
     else:
