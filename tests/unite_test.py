f911eb74ef65ebdf0dc7255e9ccac5579988cc04
Rémi <remi.rogulski@gmail.com>
2024-10-04 15:06:59 +0200

    Test Unitaire v5 - Add a prediction test - Modif test_predict in unite_test.py

--- tests/unite_test.py
+++ tests/unite_test.py
@@ -35,31 +35,30 @@ class TestScoringModel(unittest.TestCase):
         self.assertEqual(score, 11)
 
     def test_predict(self):
-        projet_7 = "/home/ec2-user/P7_Implement_a_scoring_model/models"
-        # Loading the scaler
-        scaler_path = f'{projet_7}/scaler_rawdata.dill'
-        with open(scaler_path, 'rb') as file:
-            scaler = dill.load(file)
+        # projet_7 = "/home/ec2-user/P7_Implement_a_scoring_model"
+        # # Loading the scaler
+        # scaler_path = f'{projet_7}/scaler_rawdata.dill'
+        # with open(scaler_path, 'rb') as file:
+        #     scaler = dill.load(file)
 
-        # Loading the rfe
-        rfe_path = f'{projet_7}/rfe_307511_rawdata_rfe_dill_v4_LGBM-[24-08-23 at 11_42].dill'
-        with open(rfe_path, 'rb') as file:
-            rfe = dill.load(file)
+        # # Loading the rfe
+        # rfe_path = f'{projet_7}/models/rfe_307511_rawdata_rfe_dill_v4_LGBM-[24-08-23 at 11_42].dill'
+        # with open(rfe_path, 'rb') as file:
+        #     rfe = dill.load(file)
 
-        # Loading the model
-        model_path = f'{projet_7}/307511_rawdata_rfe_dill_v4_LGBM-[24-08-23 at 11_42].dill'
-        # Ouvre le fichier en mode binaire et charge le modèle
-        with open(model_path, 'rb') as file:
-            model = dill.load(file)
+        # # Loading the model
+        # model_path = f'{projet_7}/models/307511_rawdata_rfe_dill_v4_LGBM-[24-08-23 at 11_42].dill'
+        # # Ouvre le fichier en mode binaire et charge le modèle
+        # with open(model_path, 'rb') as file:
+        #     model = dill.load(file)
 
-        # Loading the explainer
-        explainer_path = f'{projet_7}/explainer.dill'
-        # Ouvre le fichier en mode binaire et charge le modèle
-        with open(explainer_path, 'rb') as file:
-            explainer = dill.load(file)
+        # # Loading the explainer
+        # explainer_path = f'{projet_7}/models/explainer.dill'
+        # # Ouvre le fichier en mode binaire et charge le modèle
+        # with open(explainer_path, 'rb') as file:
+        #     explainer = dill.load(file)
 
-        path = 'C:/Users/remid/Documents/_OC_ParcoursDataScientist/P7_Implémentez_un_modèle_de_scoring/P7_Implement_a_scoring_model/tests'
-        data = pd.read_csv(f'{path}/test_data.csv', sep=';', index_col='SK_ID_CURR')
+        data = pd.read_csv(f'{projet_7}/tests/test_data.csv', sep=';', index_col='SK_ID_CURR')
         # Convertir en JSON
         data_json = data.to_dict(orient='records')
 
