dfb2619ca7cda23a7dbf2416dcc22b0b14f24aa2
Rémi <remi.rogulski@gmail.com>
2024-10-04 16:26:27 +0200

    Test Unitaire v15 - Add a prediction test - Modif test_predict in unite_test.py

--- API/app.py
+++ API/app.py
@@ -87,7 +87,7 @@ def welcome():
     </head>
     <body>
         <div class="content">
-            <h1>V85 - Bienvenue sur votre API !</h1>
+            <h1>V86 - Bienvenue sur votre API !</h1>
         </div>
     </body>
     </html>
